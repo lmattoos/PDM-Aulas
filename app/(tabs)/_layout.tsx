921b9dd5624b6ffa8f1caa8a7f1d37ef7ec11ed6
lmattoos <lorenzobeauvalet.pl014@academico.ifsul.edu.br>
2026-04-10 16:10:17 -0300

    feat: layout do (tabs) montado

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,46 @@
 import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+import { Icon, useTheme } from "react-native-paper";
 
-export default function TabsLayout() {
+export default function TabLayout() {
+	const theme = useTheme();
+
 	return (
 		<Tabs
-			initialRouteName="home"
 			screenOptions={{
+				tabBarActiveTintColor: theme.colors.primary,
 				headerShown: false,
+				tabBarStyle: Platform.select({
+					ios: {
+						position: "absolute",
+					},
+					default: {},
+				}),
 			}}
 		>
-			<Tabs.Screen name="home" />
-			<Tabs.Screen name="pagina2" />
+			<Tabs.Screen
+				name="home"
+				options={{
+					title: "Home",
+					tabBarIcon: ({ color }) => (
+						<Icon
+							source="account-group"
+							color={theme.colors.primary}
+							size={20}
+						/>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="menu"
+				options={{
+					title: "Menu",
+					tabBarIcon: ({ color }) => (
+						<Icon source="menu" color={theme.colors.primary} size={20} />
+					),
+				}}
+			/>
 		</Tabs>
 	);
 }
